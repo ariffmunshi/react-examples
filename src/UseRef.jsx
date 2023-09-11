1d4ac83d0155980a6ff04b631ea6aeb995e58af3
Ariff Munshi <munshicorp@gmail.com>
2023-09-11 20:19:59 +0800

    useRef: Cache previous state with useRef

--- src/UseRef.jsx
+++ src/UseRef.jsx
@@ -3,10 +3,15 @@ function UseRef() {
     const [name, setName] = useState("");
     const renderCount = useRef(0);
     const inputRef = useRef();
+    const prevName = useRef("");
 
     useEffect(() => {
         renderCount.current = renderCount.current + 1;
     });
+
+    useEffect(() => {
+        prevName.current = name;
+    }, [name]);
 
     function focus() {
         inputRef.current.focus();
@@ -22,6 +27,7 @@ function UseRef() {
             />
             <button onClick={focus}>Focus</button>
             <p>You entered: {name}</p>
+            <p>Previous value was: {prevName.current}</p>
             <p>Item been rendered: {renderCount.current} times</p>
         </>
     );
