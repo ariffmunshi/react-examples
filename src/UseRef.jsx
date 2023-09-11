9ce1bf2a12e0a0750071b3754008892d50d9c720
Ariff Munshi <munshicorp@gmail.com>
2023-09-11 20:17:13 +0800

    useRef: Create reference to element using useRef

--- src/UseRef.jsx
+++ src/UseRef.jsx
@@ -2,17 +2,25 @@ import { useState, useRef, useEffect } from "react";
 function UseRef() {
     const [name, setName] = useState("");
     const renderCount = useRef(0);
+    const inputRef = useRef();
 
     useEffect(() => {
         renderCount.current = renderCount.current + 1;
     });
+
+    function focus() {
+        inputRef.current.focus();
+    }
+
     return (
         <>
             <input
+                ref={inputRef}
                 type="text"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
             />
+            <button onClick={focus}>Focus</button>
             <p>You entered: {name}</p>
             <p>Item been rendered: {renderCount.current} times</p>
         </>
