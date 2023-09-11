aea389eddad91d9536c8f2ec48ed4d52e6ddcca7
Ariff Munshi <munshicorp@gmail.com>
2023-09-11 13:14:36 +0800

    useState: Calling useState with a callback func

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 function UseState() {
-    const [count, setCount] = useState(0);
+    // const [count, setCount] = useState(0);
+    // initialise state with callback function if only require to run once
+    const [count, setCount] = useState(() => {
+        return 0;
+    });
 
     // Pass previous value as callback if using previous value
     function decrementCount() {
